82b73b56a8eac1c6ae3fe002c7ccc6ba3acbfabc
irvin <irvin.leon@comercio.com.pe>
2017-11-23 18:04:48 -0500

    adding angular materiala components

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,12 +4,15 @@ import { VistaComponent } from './vista/vista.component';
 import { FormsModule } from '@angular/forms';
 import { RestService } from './rest.service';
 import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   declarations: [VistaComponent],
   providers : [RestService]
